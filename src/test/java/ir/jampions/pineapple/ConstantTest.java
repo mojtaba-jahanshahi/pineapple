9ad3f95b50c5c43faa721be07046528411f92f47
Alireza Pourtaghi <jampions@protonmail.com>
2018-04-11 00:51:52 +0430

    Add test for type safety check in Constant enum

---
+++ src/test/java/ir/jampions/pineapple/ConstantTest.java
@@ -0,0 +1,19 @@
+package ir.jampions.pineapple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConstantTest {
+
+    @Test
+    public void testTypeSafety() {
+        assertEquals("PINEAPPLE", Constant.BANNER_TEXT.getValue());
+        assertEquals("close", Constant.AUTO_CLOSABLE_CLEANUP_METHOD_NAME.getValue());
+        assertEquals("/tmp/", Constant.REPOSITORY_BASE_DIRECTORY.getValue());
+        assertEquals(".git", Constant.GIT_FILE_EXTENSION.getValue());
+        assertEquals(new Integer(1), Integer.valueOf(Constant.SCHEDULER_POOL_SIZE.getValue()));
+        assertEquals(new Integer(5), Integer.valueOf(Constant.SCHEDULER_INITIAL_DELAY_IN_MINUTES.getValue()));
+        assertEquals(new Integer(5), Integer.valueOf(Constant.SCHEDULER_PERIOD_IN_MINUTES.getValue()));
+    }
+}
